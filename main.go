22effd208a81798823c870d489517f38173cf4ba
g0rbe <mail@danielgorbe.com>
2023-01-27 01:52:12 +0100

    add check argument

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 
 	"github.com/elmasy-com/columbus-sdk/db"
@@ -14,22 +18,74 @@ var (
 	Commit  string
 )
 
-func main() {
+// Returns "up-to-date" and code 0, if no update required.
+// Returns the latest release version (eg.: "v0.9.1") and code 1, if update available.
+// Returns the error string and code 2, if error happened.
+func checkUpdate() {
 
-	if len(os.Args) != 2 {
-		fmt.Fprintf(os.Stderr, "Path to config file is missing!\n")
-		fmt.Printf("Usage: %s <path-to-config>\n", os.Args[0])
+	resp, err := http.Get("https://api.github.com/repos/elmasy-com/columbus-server/releases/latest")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "HTTP failed: %s\n", err)
+		os.Exit(2)
+	}
+	defer resp.Body.Close()
+
+	out, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read body: %s\n", err)
+		os.Exit(2)
+	}
+
+	var v struct {
+		TagName string `json:"tag_name"`
+	}
+
+	err = json.Unmarshal(out, &v)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to unmarshal: %s\n", err)
+		os.Exit(2)
+	}
+	if v.TagName == "" {
+		fmt.Fprint(os.Stderr, "Failed to unmarshal: TagName is empty\n")
+		os.Exit(2)
+	}
+
+	if Version < v.TagName {
+		fmt.Printf("%s", v.TagName)
 		os.Exit(1)
 	}
 
-	if os.Args[1] == "version" {
+	fmt.Printf("up-to-date")
+	os.Exit(0)
+}
+
+func main() {
+
+	path := flag.String("config", "", "Path to the config file.")
+	version := flag.Bool("version", false, "Print version informations.")
+	check := flag.Bool("check", false, "Check for updates.")
+	flag.Parse()
+
+	if *version {
 		fmt.Printf("Version: %s\n", Version)
 		fmt.Printf("Git Commit: %s\n", Commit)
 		os.Exit(0)
 	}
 
+	if *check {
+		// checkUpdate will exit inside the function
+		checkUpdate()
+	}
+
+	if *path == "" {
+		fmt.Fprintf(os.Stderr, "Path to the config file is missing!\n")
+		fmt.Printf("Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
 	fmt.Printf("Parsing config file...\n")
-	if err := config.Parse(os.Args[1]); err != nil {
+	if err := config.Parse(*path); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to parse config file: %s\n", err)
 		os.Exit(1)
 	}
