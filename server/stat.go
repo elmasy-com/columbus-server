c2e6b08aa7ab0fa5adb1150e2b9b684481bdeb52
g0rbe <mail@danielgorbe.com>
2023-05-20 04:28:17 +0200

    remove unique stats

--- server/stat.go
+++ server/stat.go
@@ -12,30 +12,22 @@ import (
 )
 
 type Stat struct {
-	Date          int64
-	Totalnum      int64
-	TldNum        int64
-	DomainNum     int64
-	FullDomainNum int64
-	SubNum        int64
-	m             sync.Mutex
+	Date     int64
+	Totalnum int64
+	m        sync.Mutex
 }
 
 var (
 	Current Stat
 )
 
-func (s *Stat) Update(TotalNum int64, TldNum int64, DomainNum int64, FullDomainNum int64, SubNum int64) {
+func (s *Stat) Update(TotalNum int64) {
 
 	s.m.Lock()
 	defer s.m.Unlock()
 
 	s.Date = time.Now().Unix()
 	s.Totalnum = TotalNum
-	s.TldNum = TldNum
-	s.DomainNum = DomainNum
-	s.FullDomainNum = FullDomainNum
-	s.SubNum = SubNum
 }
 
 func (s *Stat) GetDate() int64 {
@@ -54,44 +46,12 @@ func (s *Stat) GetTotalNum() int64 {
 	return s.Totalnum
 }
 
-func (s *Stat) GetTldNum() int64 {
-
-	s.m.Lock()
-	defer s.m.Unlock()
-
-	return s.TldNum
-}
-
-func (s *Stat) GetDomainNum() int64 {
-
-	s.m.Lock()
-	defer s.m.Unlock()
-
-	return s.DomainNum
-}
-
-func (s *Stat) GetFullDomainNum() int64 {
-
-	s.m.Lock()
-	defer s.m.Unlock()
-
-	return s.FullDomainNum
-}
-func (s *Stat) GetSubNum() int64 {
-
-	s.m.Lock()
-	defer s.m.Unlock()
-
-	return s.SubNum
-}
-
 func (s *Stat) IsEmpty() bool {
 
 	s.m.Lock()
 	defer s.m.Unlock()
 
-	return s.Date == 0 && s.Totalnum == 0 && s.TldNum == 0 &&
-		s.DomainNum == 0 && s.FullDomainNum == 0 && s.SubNum == 0
+	return s.Date == 0 && s.Totalnum == 0
 }
 
 // UpdateStat is created to run as a goroutine.
@@ -99,19 +59,19 @@ func (s *Stat) IsEmpty() bool {
 // Updates the Current variable every 60 minutes and updates the unique collection via db.UpdateUniques() every config.StatAPIWait minutes.
 func UpdateStat() {
 
-	ticker := time.NewTicker(60 * time.Minute)
+	ticker := time.NewTicker(6 * time.Hour)
 
 	// Update stats at the beginning
-	if total, tlds, domains, fullDomains, subs, err := db.GetStat(); err == nil {
-		Current.Update(total, tlds, domains, fullDomains, subs)
+	if total, err := db.GetStat(); err == nil {
+		Current.Update(total)
 	} else {
 		fmt.Fprintf(os.Stderr, "Failed to get DB stat: %s\n", err)
 	}
 
 	for range ticker.C {
 
-		if total, tlds, domains, fullDomains, subs, err := db.GetStat(); err == nil {
-			Current.Update(total, tlds, domains, fullDomains, subs)
+		if total, err := db.GetStat(); err == nil {
+			Current.Update(total)
 		} else {
 			fmt.Fprintf(os.Stderr, "Failed to get DB stat: %s\n", err)
 		}
@@ -127,7 +87,5 @@ func StatGet(c *gin.Context) {
 
 	// Return a copy only.
 	// This was the easiest way to control the write (update) / read process with the mutex.
-	c.JSON(http.StatusOK,
-		gin.H{"date": Current.GetDate(), "total": Current.GetTotalNum(), "tld": Current.GetTldNum(),
-			"domain": Current.GetDomainNum(), "fulldomain": Current.GetFullDomainNum(), "sub": Current.GetSubNum()})
+	c.JSON(http.StatusOK, gin.H{"date": Current.GetDate(), "total": Current.GetTotalNum()})
 }
